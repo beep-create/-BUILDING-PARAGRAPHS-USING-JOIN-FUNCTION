e5dce4f94ed92e834119bde96fa7f47f98ac23c4
beep-create <77546458+beep-create@users.noreply.github.com>
2021-04-14 13:06:24 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+function showparagraph1()
+{
+    var inputs = [];
+    for (var i = 1 ; i <=6 ; i++)
+    {
+        inputs.push(document.getElementById("name_of_the_1" + i).value);
+    }
+    inputs.join(".");
+    document.getElementById("showparagraph1").innerHTML = inputs.join(".");
+    console.log("showparagraph1");
+
+    {
+
+        inputs.push(document.getElementById("name_of_the_2" + i).value);
+    }
+
+    inputs.join(".");
+    document.getElementById("showparagraph1").innerHTML = inputs.join(".");
+    console.log("showparagraph1");
+    {
+        inputs.push(document.getElementById("name_of_the_3" + i).value);
+    }
+
+    inputs.join(".");
+    document.getElementById("showparagraph1").innerHTML = inputs.join(".");
+    console.log("showparagraph1");
+    {
+        inputs.push(document.getElementById("name_of_the_4" + i).value);
+    }
+
+    inputs.join(".");
+    document.getElementById("showparagraph1").innerHTML = inputs.join(".");
+    console.log("showparagraph1");
+    {
+        inputs.push(document.getElementById("name_of_the_5" + i).value);
+    }
+
+    inputs.join(".");
+    document.getElementById("showparagraph1").innerHTML = inputs.join(".");
+    console.log("showparagraph1");
+    {
+        inputs.push(document.getElementById("name_of_the_6" + i).value);
+    }
+    
+}
+function showparagraph2()
+{
+    var inputs = [];
+    for (var i = 1 ; i <=6 ; i++)
+    {
+        inputs.push(document.getElementById("name_of_the_7" + i).value);
+        console.log("showparagraph2");
+    }
+
+    inputs.join(".");
+    document.getElementById("showparagraph2").innerHTML = inputs.join(".");
+
+    {
+        inputs.push(document.getElementById("name_of_the_8" + i).value);
+        console.log("showparagraph2");
+    }
+
+    inputs.join(".");
+    document.getElementById("showparagraph2").innerHTML = inputs.join(".");
+    {
+        inputs.push(document.getElementById("name_of_the_9" + i).value);
+        console.log("showparagraph2");
+    }
+
+    inputs.join(".");
+    document.getElementById("showparagraph2").innerHTML = inputs.join(".");
+    {
+        inputs.push(document.getElementById("name_of_the_10" + i).value);
+        console.log("showparagraph2");
+    }
+
+    inputs.join(".");
+    document.getElementById("showparagraph2").innerHTML = inputs.join(".");
+    {
+        inputs.push(document.getElementById("name_of_the_11" + i).value);
+        console.log("showparagraph2");
+    }
+
+    inputs.join(".");
+    document.getElementById("showparagraph2").innerHTML = inputs.join(".");
+    {
+        inputs.push(document.getElementById("name_of_the_12" + i).value);
+        console.log("showparagraph2");
+    }
+    
+    
+}
